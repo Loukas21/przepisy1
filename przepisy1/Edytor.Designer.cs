2d4f926a3d4a74c9ac7767b3f87ef0d9173f1941
Loukas21 <lukaszgrzegorzskibinski@gmail.com>
2016-09-10 18:12:20 +0200

    Gałąź odpowiedzialna za bazę danych

--- przepisy1/Edytor.Designer.cs
+++ przepisy1/Edytor.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(510, 12);
+            this.button1.Location = new System.Drawing.Point(510, 48);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(181, 23);
             this.button1.TabIndex = 0;
