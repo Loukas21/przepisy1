dedb3042e9bf410d4beaaf13e64d18525d7d6e8d
Loukas21 <lukaszgrzegorzskibinski@gmail.com>
2016-10-01 23:00:14 +0200

    Dodanie opcji zachowania proporcji obrazu (Size mode - zoom)

--- przepisy1/Edytor.Designer.cs
+++ przepisy1/Edytor.Designer.cs
@@ -123,7 +123,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(24, 163);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(200, 200);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
             // 
