0624ab1123fc6159d7445dd92c4615b7715c7a54
Loukas21 <lukaszgrzegorzskibinski@gmail.com>
2016-10-01 21:27:11 +0200

    Dodano obsługę pustych okien formularza dodawania przepisu oraz dodano obsługę ładowania obrazka z dysku do PictureBox

--- przepisy1/Edytor.Designer.cs
+++ przepisy1/Edytor.Designer.cs
@@ -42,6 +42,8 @@
             this.label6 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -167,6 +169,7 @@
             this.button2.TabIndex = 5;
             this.button2.Text = "Dodaj zdjęcie...";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -176,12 +179,26 @@
             this.button3.TabIndex = 6;
             this.button3.Text = "Dodaj przepis";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(240, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 12;
             // 
             // Edytor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(790, 414);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label6);
@@ -220,5 +237,7 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label7;
     }
 }
--- przepisy1/Edytor.cs
+++ przepisy1/Edytor.cs
@@ -15,11 +15,51 @@ namespace przepisy1
             InitializeComponent();
         }
 
+        private string sprawdz_dane()
+        {
+            string a="Wszystkie dane wprowadzono poprawnie";
+            if (textBox1.Text == "")
+            { 
+                a="Pole z nazwą przepisu nie może pozostać puste!";
+                return a;
+            }
+            if (textBox2.Text == "")
+            { 
+                a="Pole z tagami nie może pozostać puste!";
+                return a;
+            }
+            if (richTextBox1.Text == "")
+            { 
+                a="Pole ze składnikami nie może pozostać puste!";
+                return a;
+            }
+            if (richTextBox2.Text == "")
+            {
+                a = "Pole ze sposobem przygotowania nie może pozostać puste!";
+                return a;
+            }
+            return a;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             this.Hide();
             Menu ss = new Menu();
             ss.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label7.Text = sprawdz_dane();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.CheckFileExists==true)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
         }
     }
 }
